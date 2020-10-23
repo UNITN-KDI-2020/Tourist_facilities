5e4c2e6482a8c0b330d8415e5e2225234a5e0e35
javierpe27 <49977188+javierpe27@users.noreply.github.com>
2020-10-23 12:04:48 +0200

    Add files via upload

---
+++ dataset/Scope Definition & Inception/data/skiStaticInfo.py
@@ -0,0 +1,30 @@
+import scrapy
+from pandas._libs import json
+
+
+class skiScraper(scrapy.Spider):
+    name = "SkyResorts_spider"
+    start_urls = ['https://www.skiresort.info/ski-resorts/trentino/']
+
+    def parse (self, response):
+        self.logger.info("HELOOOO")
+        ski_items = response.css('div.resort-list-item-image--big')
+
+        for resort in ski_items:
+            name = resort.css('a.h3::text').getall()[0]
+            name = name.replace("/\u200b", "").replace("\u2013", "").replace("\u00e0", "").replace("\u00f9","").replace("\u00e8","")
+            yield {
+
+                'name': name,
+                'Total lenght': resort.css('span.slopeinfoitem::text').getall()[0],
+                'km blue slope': resort.css('span.slopeinfoitem::text').getall()[1],
+                'km red slope': resort.css('span.slopeinfoitem::text').getall()[2],
+                'km black slope': resort.css('span.slopeinfoitem::text').getall()[3],
+                'lifts': resort.css('li::text').getall()[0],
+                'price': resort.css('td::text').getall()[len(resort.css('td::text').getall()) - 1].replace("\u20ac","")
+            }
+
+
+
+
+
